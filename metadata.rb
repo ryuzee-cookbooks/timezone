c9a3d22b27a15881dc4e5dc44adee260ef7f92e5
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-24 06:40:00 +0900

    clarify license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "timezone"
 maintainer       "Ryuzee"
 maintainer_email "ryuzee@gmail.com"
-license          "MIT License"
+license          "MIT"
 description      "Configure Timezone for JST"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
