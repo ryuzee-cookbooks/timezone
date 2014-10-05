5a1bafcae6ca4208ae58cefea4571450862f8d40
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-10-05 09:52:58 +0900

    fix for serverspec 2.0

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -1,13 +1,4 @@
-require 'serverspec'
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
-  c.path = "/sbin:/usr/sbin"
-end
+require 'spec_helper'
 
 describe command( "date 2>/dev/null | grep 'JST'" ) do
   it { should return_exit_status 0 }
